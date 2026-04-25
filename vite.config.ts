1535e7e7eb66cd844080acd09d9d9d461f8d9185
Adilet <pranov.adiletqwe@gmail.com>
2026-04-25 16:30:30 +0600

    Refactored Vite configuration to simplify build settings. Removed unnecessary library formats and external dependencies, while retaining ESNext target for improved compatibility.

--- vite.config.ts
+++ vite.config.ts
@@ -4,34 +4,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    target: ["esnext"],
-    lib: {
-      entry: "./src/index.ts",
-      formats: ["cjs", "es", "umd"],
-      name: "super-timeline",
-      fileName: "index",
-    },
-    rollupOptions: {
-      external: [
-        "react",
-        "react-dom",
-        "zustand",
-        "fabric",
-        "@remotion/cli ",
-        "@remotion/player",
-        "typescript-eslint",
-        "eslint-plugin-react-hooks",
-        "eslint-plugin-react-refresh",
-        "remotion",
-        "eslint",
-      ],
-    },
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    target: "esnext",
   },
 });
